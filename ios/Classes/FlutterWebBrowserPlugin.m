9e77a2cf58976f4273ed6a8d0795246caab41497
Jacob Phillips <hello@mrjacobphillips.com>
2020-03-11 21:12:00 -0400

    Added color control for SFSafariViewController

--- ios/Classes/FlutterWebBrowserPlugin.m
+++ ios/Classes/FlutterWebBrowserPlugin.m
@@ -3,9 +3,7 @@
 
 @implementation FlutterWebBrowserPlugin 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"flutter_web_browser"
-            binaryMessenger:[registrar messenger]];
+  FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"flutter_web_browser" binaryMessenger:[registrar messenger]];
   FlutterWebBrowserPlugin* instance = [[FlutterWebBrowserPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
@@ -13,14 +11,45 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     if ([@"openWebPage" isEqualToString:call.method]) {
         NSString *url = call.arguments[@"url"];
+        NSString *barTint = call.arguments[@"ios_bar_tint_color"];
+        NSString *controlTint = call.arguments[@"ios_control_tint_color"];
+        
         NSURL *URL = [NSURL URLWithString:url];
+        UIColor *barTintColor = nil;
+        UIColor *controlTintColor = nil;
+
+        if ( ( ![barTint isEqual:[NSNull null]] ) && ( [barTint length] != 0 ) ) {
+            barTintColor = [self colorFromHexString:barTint];
+        }
+        
+        if ( ( ![controlTint isEqual:[NSNull null]] ) && ( [controlTint length] != 0 ) ) {
+            controlTintColor = [self colorFromHexString:controlTint];
+        }
+        
         UIViewController *viewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
         if ( viewController.presentedViewController && !viewController.presentedViewController.isBeingDismissed ) {
             viewController = viewController.presentedViewController;
         }
         
-        if (@available(iOS 9.0, *)) {
+        if (@available(iOS 10.0, *)) {
             SFSafariViewController *sfvc = [[SFSafariViewController alloc] initWithURL:URL];
+            
+            if ( ( ![barTint isEqual:[NSNull null]] ) && ( [barTint length] != 0 ) ) {
+                sfvc.preferredBarTintColor = barTintColor;
+            }
+            
+            if ( ( ![controlTint isEqual:[NSNull null]] ) && ( [controlTint length] != 0 ) ) {
+                sfvc.preferredControlTintColor = controlTintColor;
+            }
+            
+            [viewController presentViewController:sfvc animated:YES completion:nil];
+        } else if (@available(iOS 9.0, *)) {
+            SFSafariViewController *sfvc = [[SFSafariViewController alloc] initWithURL:URL];
+            
+            if ( ( ![controlTintColor isEqual:[NSNull null]] ) && ( [controlTint length] != 0 ) ) {
+                sfvc.view.tintColor = controlTintColor;
+            }
+            
             [viewController presentViewController:sfvc animated:YES completion:nil];
         } else {
             [[UIApplication sharedApplication] openURL:URL];
@@ -29,6 +58,14 @@
     } else {
         result(FlutterMethodNotImplemented);
     }
+}
+
+- (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:3]; // bypass '#' character and leading FFs
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 
--- lib/flutter_web_browser.dart
+++ lib/flutter_web_browser.dart
@@ -1,20 +1,21 @@
 import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class FlutterWebBrowser {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_web_browser');
+  static MethodChannel _channel = MethodChannel('flutter_web_browser');
 
-  static Future<dynamic> openWebPage({url, androidToolbarColor}) {
-    var hexColor;
-    if (androidToolbarColor != null) {
-      hexColor =
-          '#' + androidToolbarColor.value.toRadixString(16).padLeft(8, '0');
-    }
+  static Future<dynamic> openWebPage({url, androidToolbarColor, iOSBarTintColor, iOSControlTintColor}) {
+    var androidToolbarHexColor, iOSBarHexColor, iOSControlHexColor;
+
+    if (androidToolbarColor != null) {androidToolbarHexColor = '#' + androidToolbarColor.value.toRadixString(16).padLeft(8, '0');}
+    if (iOSBarTintColor != null) {iOSBarHexColor = '#' + iOSBarTintColor.value.toRadixString(16).padLeft(8, '0');}
+    if (iOSControlTintColor != null) {iOSControlHexColor = '#' + iOSControlTintColor.value.toRadixString(16).padLeft(8, '0');}
+
     return _channel.invokeMethod('openWebPage', {
       "url": url,
-      "android_toolbar_color": hexColor,
+      "android_toolbar_color": androidToolbarHexColor,
+      "ios_bar_tint_color": iOSBarHexColor,
+      "ios_control_tint_color": iOSControlHexColor,
     });
   }
 }
